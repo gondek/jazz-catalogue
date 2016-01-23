ffa01326bec0f0cb214246571b4614f03f0d88ec
Alex Gondek <alexgondek@gmail.com>
2016-01-22 21:13:11 -0800

    Addeded the missing branch switch in the deployment script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -13,12 +13,15 @@ if [[ \"`git status --porcelain`\" != \"\" ]]; then
   exit 1
 fi
 
+git checkout gh-pages
 cp -r _site/* .
 git add -A
 MSG=$(git log --format=%B -n 1 HEAD)
-git commit -m \"$MSG\"
-echo "Site files commited! Pushing: ....\n\n"
+git commit -m "$MSG"
+echo -e "Site files commited! Pushing: ....\n\n"
 
 git push origin gh-pages
 git checkout master
 git clean -fd
+
+echo -e "\n\nDone!\n\n"
