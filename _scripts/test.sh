86417c0a0f9fd3090a08df8bbe680df5bc311b14
Alex Gondek <alexgondek@gmail.com>
2016-02-13 10:56:57 -0800

    Basic CI/tests setup

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -2,9 +2,5 @@
 set -e
 cd `dirname $0`/../
 
-# ensure images exist for each post
-
-./_scripts/thumbnails.sh
-echo "Thumbnails Generated!"
-
-jekyll build --quiet
+# does the site build?
+./_scripts/build.sh
