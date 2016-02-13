03b3f600e23ac9a62e0e5b8836495b6542a7b4c6
Alex Gondek <alexgondek@gmail.com>
2016-02-13 11:26:00 -0800

    Added image checking to CI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Alex's Jazz Catalogue
 
+[![Build Status](https://travis-ci.org/gondek/jazz-catalogue.svg?branch=master)](https://travis-ci.org/gondek/jazz-catalogue)
+
 ## Todo
 - review the reviews
 - add logo
--- _scripts/test.sh
+++ _scripts/test.sh
@@ -3,4 +3,22 @@ set -e
 cd `dirname $0`/../
 
 # does the site build?
-./_scripts/build.sh
+./_scripts/build.sh >/dev/null
+echo "[PASS 01] Site built correctly!"
+
+# are all album covers 300x300 and contain no metadata?
+for f in assets/covers/*; do
+  SIZE=$(exiftool $f | grep "Image Size")
+  if [[ "$SIZE" != *"300x300"* ]]
+  then
+    echo -e "[FAIL 02] $f has the wrong dimensions:\n $SIZE";
+    exit 1;
+  fi
+done
+METADATA=$(exiftool -all= assets/covers/*)
+if [[ "$METADATA" != *"0 image files updated"* ]]
+then
+  echo -e "[FAIL 02] Images contain metadata:\n $METADATA";
+  exit 1;
+fi
+echo "[PASS 02] Images are correctly formatted"
