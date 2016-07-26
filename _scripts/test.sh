df43f9111277373b6949312f040bbc7a1166a2e8
Alex Gondek <alexgondek@gmail.com>
2016-07-25 19:08:02 -0700

    Changed build script formatting

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -3,10 +3,13 @@ set -e
 cd `dirname $0`/../
 
 # does the site build?
+echo "[TASK] Building site ----------------------------------------------------"
 ./_scripts/build.sh >/dev/null
 echo "[PASS] Site built correctly! --------------------------------------------"
+echo
 
 # are all album covers 300x300 and contain no metadata?
+echo "[TASK] Checking images --------------------------------------------------"
 for f in assets/covers/*; do
   SIZE=$(exiftool $f | grep "Image Size")
   if [[ "$SIZE" != *"300x300"* ]]
@@ -22,11 +25,16 @@ then
   exit 1;
 fi
 echo "[PASS] Images are correctly formatted -----------------------------------"
+echo
 
 # does the site HTML validate?
+echo "[TASK] Checking HTML ----------------------------------------------------"
 htmlproofer --assume-extension --check-html ./_site
 echo "[PASS] Site markup and links passed validation --------------------------"
+echo
 
 # are all the youtube links still available?
+echo "[TASK] Checking Youtube Links -------------------------------------------"
 ./_scripts/check_youtube.sh
 echo "[PASS] All youtube links are working ------------------------------------"
+echo
