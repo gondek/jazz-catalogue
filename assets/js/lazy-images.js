0a4545762f5f80367f5891602bb3c8efef2eb14d
Alex Gondek <alexgondek@gmail.com>
2018-09-06 19:33:32 -0700

    Changed "About" page and loading javascript

--- assets/js/lazy-images.js
+++ assets/js/lazy-images.js
@@ -1,9 +1,15 @@
 /* global $ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 $(document).ready(function () {
   $('img.lazy-load').removeClass('hide').unveil(200, function () {
     $(this).on('load', function () {
-      $(this).removeClass('lazy-load')
-      $(this).siblings('.lazy-load').removeClass('lazy-load')
+      var that = $(this)
+      setInterval(function () {
+        that.removeClass('lazy-load')
+      }, getRandomInt(0, 4) * 50 + 200)
     })
   })
 })
